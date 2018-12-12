ee75233aec2dea3485e3ea5fef3fef06aa5f77cb
PBread07 <45771939+PBread07@users.noreply.github.com>
2018-12-12 23:44:38 +0000

    Update botcode.py

--- botcode.py
+++ botcode.py
@@ -102,7 +102,7 @@ async def on_message(message):
         embed.set_thumbnail(url='https://vignette.wikia.nocookie.net/2007scape/images/5/5c/Corporeal_Beast.png/revision/latest?cb=20160212005921')
 
         embed.add_field(
-            name="**Suggested Inventory Setup:**", value='Void Melee Inv: http://prntscr.com/lu8mwl\n', inline=False)
+            name="**Suggested Inventory Setup:**", value='Void Melee Inv: http://prntscr.com/lu8mwl\n*Special weapons from best to worst DWH>BGS>Arclight>Crystal Halberd', inline=False)
         embed.set_footer(text=event_time_EST + ' | ' + event_time_GMT)
         embed.set_thumbnail(url='https://vignette.wikia.nocookie.net/2007scape/images/5/5c/Corporeal_Beast.png/revision/latest?cb=20160212005921')
         old_msg=await client.send_message(message.channel, embed=embed)

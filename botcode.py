c0eb1fc515d6243a3d3f03b6b75ae32ba41f7913
PBread07 <45771939+PBread07@users.noreply.github.com>
2018-12-12 17:48:56 +0000

    Update botcode.py

--- botcode.py
+++ botcode.py
@@ -52,7 +52,11 @@ async def on_message(message):
             event_datetime + timedelta(hours=-4), '%d %b %Y %I:%M%p') + ' EST'
 
         embed = discord.Embed(title="!Event:", color=0x00ff00)
-        embed.add_field(name="Suggested Gear:", value='**80+ Hitpoints**\n**80+ Attack**\n**70+ Strength (Required, 80+ Recommended)**\n**70+ Defence (90+ if Tank)**\n**80+ Range**\n**70+ Magic (75+ Recommended)**\n**44+ Prayer (Eagle Eye, 70+ Piety, 74+ Rigour)**', inline=False)
+        embed.add_field(
+            name="Requirements", value='**- 80+ Hitpoints**\n**- 80+ Attack**\n**- 70+ Strength (Required, 80+ Recommended)**\n**- 70+ Defence (90+ if Tank)**', inline=False)
+        embed.set_footer(text=event_time_EST + ' | ' + event_time_GMT)
+        embed.set_thumbnail(url=message.author.avatar_url)
+        old_msg=await client.send_message(message.channel, embed=embed)
 
 
 @client.event
